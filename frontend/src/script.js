5312abaf47236ad10be5c7a5437b00c2cee13122
P-3142857 <144462891+P-3142857@users.noreply.github.com>
2023-09-15 18:21:58 +0900

    Update script.js
    always show only one value (remove old value everytime new onclick happens)

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,4 +1,6 @@
 function loadtextbyid(element){
+    var cleanshit = document.getElementById("prev");
+    cleanshit.remove();
     var parent = element.parentNode;
     //alert(parent.id);
     var content = parent.querySelector("div");
@@ -6,6 +8,7 @@ function loadtextbyid(element){
     console.log(element.id);
     var bottom = document.querySelector("body");
     let desc = document.createElement("div");
+    desc.id = "prev";
     desc.innerHTML = '<p>'+element.id+'</p>';
     bottom.appendChild(desc);
     
